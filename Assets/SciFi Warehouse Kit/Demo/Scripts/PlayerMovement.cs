6b505c4d0eb9b02d8c46f9f72bc534ec5b471b07
Lufku <adrivm133@gmail.com>
2026-09-20 18:40:52 +0200

    Initial commit

---
+++ Assets/SciFi Warehouse Kit/Demo/Scripts/PlayerMovement.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    [Header("Movement")]
+    public float speed = 8f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    [Header("Ground Check")]
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    [Header("Mouse Look")]
+    public Transform playerCamera;
+    public float mouseSensitivity = 100f;
+
+    [Header("Audio")]
+    public AudioClip footStepSound;
+    public float footStepDelay;
+
+    private float nextFootstep = 0;
+    private float xRotation = 0f;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        // FIRST PERSON CAMERA
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        playerCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+
+        // GROUND CHECK
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        // MOVEMENT
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 motion = transform.right * x + transform.forward * z;
+        controller.Move(motion * speed * Time.deltaTime);
+
+        // JUMP
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        // GRAVITY
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        // FOOTSTEPS
+        if ((Input.GetKey(KeyCode.W) ||
+             Input.GetKey(KeyCode.A) ||
+             Input.GetKey(KeyCode.S) ||
+             Input.GetKey(KeyCode.D)) &&
+             isGrounded)
+        {
+            nextFootstep -= Time.deltaTime;
+
+            if (nextFootstep <= 0)
+            {
+                GetComponent<AudioSource>().PlayOneShot(footStepSound, 0.7f);
+                nextFootstep = footStepDelay;
+            }
+        }
+    }
+}
